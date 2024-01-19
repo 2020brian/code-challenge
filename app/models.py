9925ed30151926666f69479b0599e5ffa34cbbbe
onjorobrian <onjorobrian1@gmail.com>
2024-01-19 12:28:11 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -23,9 +24,16 @@ class Powers(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
 
     # Many-to-Many relationship with HeroPowers
     hero_powers = db.relationship('HeroPowers', back_populates='power')
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not description or len(description) < 20:
+            raise ValueError("Description must be present and at least 20 characters long")
+        return description
 
 class HeroPowers(db.Model):
     __tablename__ = 'hero_powers'
@@ -34,7 +42,15 @@ class HeroPowers(db.Model):
 
     hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    strength = db.Column(db.String(10), nullable=False)
 
     # Define relationships
     hero = db.relationship('Hero', back_populates='hero_powers')
     power = db.relationship('Powers', back_populates='hero_powers')
+
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        allowed_strengths = ['Strong', 'Weak', 'Average']
+        if strength not in allowed_strengths:
+            raise ValueError(f"Invalid strength value. Allowed values are {', '.join(allowed_strengths)}")
+        return strength
